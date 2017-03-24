19112e73ea6e34b2c40759a4b5fed916a22aa503
Aaron <amartinezdasilva@danielcastelao.org>
2017-03-24 11:01:02 +0100

    proyecto acabado

--- app/src/main/java/com/example/aaron/proyecto3/MainActivity.java
+++ app/src/main/java/com/example/aaron/proyecto3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aaron.proyecto3;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -18,6 +20,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +30,8 @@ import com.example.aaron.proyecto3.R;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_17;
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -35,7 +40,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    JSONObject clienteJS, jsonm;
+    public static String usuario;
     private WebSocketClient mWebSocketClient;
 
     private static final int MY_PERMISSIONS_REQUEST_INTERNET=1;
@@ -113,9 +119,35 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.user) {
+            AlertDialog.Builder alert= new AlertDialog.Builder(this);
+            final EditText usern=new EditText(this);
+            usern.setSingleLine();
+            usern.setPadding(50,0,50,0);
+            alert.setTitle("USERNAME");alert.setMessage("Introduzca su nombre de usuario");alert.setView(usern);
+            alert.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                 usuario=usern.getText().toString();
+                }
+            });
+            alert.create();
+            alert.show();
 
-        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.conexion) {
+                if(usuario==null){
+                AlertDialog.Builder alertC=new AlertDialog.Builder(this);
+                alertC.setTitle("USERNAME necesario");alertC.setMessage("Necesitamos saber tu nombre de usuario para conectarte ");
+                alertC.setPositiveButton("Aceptar",null);
+                alertC.create();
+                alertC.show();
+
+            }else{
+                connectWebSocket();
+
+            }
+
 
         } else if (id == R.id.nav_manage) {
 
@@ -147,7 +179,8 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onOpen(ServerHandshake serverHandshake) {
                 Log.i("Websocket", "Opened");
-                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+
+                mWebSocketClient.send("{\"id\":\"" + usuario + "\"}");   clienteJS = new JSONObject();
             }
 
             @Override
@@ -157,7 +190,29 @@ public class MainActivity extends AppCompatActivity
                     @Override
                     public void run() {
                         TextView textView = (TextView)findViewById(R.id.textView);
-                        textView.setText(textView.getText() + "\n" + message);
+
+                        String nick;
+                        String mens;
+                        String destino;
+                        int priv;
+                       try {
+                            clienteJS = new JSONObject(message);
+                            nick= clienteJS.getString("id");
+                            mens = clienteJS.getString("mensaje");
+                            destino= clienteJS.getString("destino");
+                            priv= clienteJS.getInt("Privado");
+
+                            if(priv==1){
+                                if(destino.equals(usuario)){
+                                    textView.setText(textView.getText() + "\n" + nick+ "\n" + mens);
+                                }
+
+                            }
+                        } catch (JSONException e) {
+                           textView.setText(textView.getText() + "\n" + message);
+                        }
+
+
                     }
                 });
             }
@@ -177,8 +232,32 @@ public class MainActivity extends AppCompatActivity
 
     }
     public void sendMessage() {
-        EditText editText = (EditText)findViewById(R.id.message);
-        mWebSocketClient.send(editText.getText().toString());
-        editText.setText("");
+        EditText mens = (EditText)findViewById(R.id.message);
+        EditText destino = (EditText)findViewById(R.id.destino);
+        CheckBox checkBox = (CheckBox)findViewById(R.id.priv);
+
+        int priv;
+
+        String destin = destino.getText().toString();
+        String men = mens.getText().toString();
+        if(checkBox.isChecked()) {
+           priv=1 ;
+        }else{
+            priv = 0;
+        }
+        jsonm = new JSONObject();
+        try {
+            clienteJS.put("id",usuario);
+            clienteJS.put("msg",mens);
+            clienteJS.put("esPrivado",priv);
+            clienteJS.put("dst",destino);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        mWebSocketClient.send(jsonm.toString());
+
+
     }
 }
